76aa4cc7857d4ff31aeb51732a76a11c27dfa9e6
Marilyn0034 <mafc.2016@gmail.com>
2025-06-10 11:51:18 -0400

    Funcion qlera

--- Tareas/Sistema de Cálculo de Promedios Estudiantiles/Funciones.py
+++ Tareas/Sistema de Cálculo de Promedios Estudiantiles/Funciones.py
@@ -1,4 +1,7 @@
 def calcularPromedio(notas, cantNotas):
-    promedio = 0.0
     promedio = int((notas/ cantNotas) * 10 + 0.5) / 10
     return(promedio)
+
+def escribir(estudiante, promedio):
+    archivo = open("Notas Curso.txt", "w")
+    archivo.write(f"{estudiante}, {promedio}")
