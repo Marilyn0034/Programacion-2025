3a8aadc950cd841ce030d58d49f2e78e0769f30f
Marilyn0034 <mafc.2016@gmail.com>
2025-07-30 13:48:39 -0400

    PTMREEEEEEEEEEE

---
+++ Tareas/Ejercicio (gastos).py
@@ -0,0 +1,27 @@
+gastos = []
+run = True
+gastos_grandes_total = 0
+limite = 20
+i = 1
+
+print("Hola! este programa se encargará de filtrar sus gastos grandes (mayores a 20 dolares) y darle una suma final de todos estos gastos.")
+print("Cuando ya no quiera ingresar más gastos, ingrese 0.")
+
+while run == True:
+    ingreso = int(input(f"Ingrese el gasto (en dólares) numero {i}:"))
+    if ingreso == 0:
+        print("Saliendo del ingreso de gastos...")
+        run = False
+    elif ingreso < 0:
+        print("No puede ingresar un gasto negativo.")
+    else:
+        gastos.append(ingreso)
+    i = i + 1
+print(gastos)
+for e in range(gastos):
+    print(e)
+    if gastos[e] > limite:
+        print(gastos[e])
+        gastos_grandes_total = gastos_grandes_total + gastos[e]
+
+print(f"Sus gastos grandes en total suman: {gastos_grandes_total}")
