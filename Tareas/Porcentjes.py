53d6288ba64086f872e21d1508fe39711a8d506a
Marilyn0034 <mafc.2016@gmail.com>
2025-09-26 14:01:08 +0000

    new file:   Tareas/Porcentjes.py

---
+++ Tareas/Porcentjes.py
@@ -0,0 +1,23 @@
+#Declaración de variables.
+run = True
+sumaDatos = 0
+porcentajes = []
+valores = []
+i = 1
+
+#Bucle While para cantidad de datos
+while run == True:
+    valor = int(input(f"Ingrese el dato número {i}:"))
+    i = i + 1
+    valores.append(valor)
+    sumaDatos = sumaDatos + valor
+    if valor == 0:
+        run = False
+
+#Calculos
+for e in valores:
+    porcentaje = e / sumaDatos * 100
+    porcentajes.append(str(porcentaje) + "%")
+
+#Salida de datos
+print(f"Los porcentajes son: {str(porcentajes)}")
